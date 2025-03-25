fe9ad0f9a15d93f7f363424d06d18d7ded09b132
haide <haider.altaf1996@gmail.com>
2025-03-25 13:28:14 -0400

    revert

--- personal_web/settings.py
+++ personal_web/settings.py
@@ -143,16 +143,3 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'chess_elo_cache_table',
-    }
-}
-
-# Development override
-if DEBUG:
-    CACHES['default'] = {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'chess-elo-dev',
-    }
